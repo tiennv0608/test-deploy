e624ec177f9568ca1b683156d5433545305e5856
tiennv0608 <tiennv0608@gmail.com>
2021-08-03 22:50:22 +0700

    b2

---
+++ 02-customer-management/src/main/java/com/codegym/demo/service/customer/CustomerService.java
@@ -0,0 +1,34 @@
+package com.codegym.demo.service.customer;
+
+import com.codegym.demo.model.Customer;
+import com.codegym.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService implements ICustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
---
+++ 02-customer-management/src/main/java/com/codegym/demo/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.demo.service.customer;
+
+import com.codegym.demo.model.Customer;
+import com.codegym.demo.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
