f644f297028e607ae669d895df9262a5f9ad437c
tiennv0608 <tiennv0608@gmail.com>
2021-08-23 15:15:14 +0700

    hỗ trợ frontend

--- 03-product-management/src/main/java/com/codegym/demo/controller/ProductController.java
+++ 03-product-management/src/main/java/com/codegym/demo/controller/ProductController.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
-@CrossOrigin("*")
 public class ProductController {
     @Autowired
     private IProductService productService;
